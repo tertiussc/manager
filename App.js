6bfc9c28b1300d533a5dc1a98167ff080d305701
Tertius Scheepers <Tertiussc@gmail.com>
2020-11-04 10:59:47 +0200

    added firebase and app is working

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import firebase from 'firebase'
+import reducers from './src/reducers';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBqUYvBOTC-06PJMh7KpwzjgYMQzHzsOIk',
+      authDomain: 'manager-4b38c.firebaseapp.com',
+      databaseURL: 'https://manager-4b38c.firebaseio.com',
+      projectId: 'manager-4b38c',
+      storageBucket: 'manager-4b38c.appspot.com',
+      messagingSenderId: '285761323821',
+      appId: '1:285761323821:web:8c4d6f19383ef9129749a4',
+      measurementId: 'G-EJ9BJS2MM4'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <Text style={{ marginTop: 20 }}>Hello!</Text>
+        </View>
+      </Provider>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
