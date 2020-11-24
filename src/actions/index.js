d6915c0c5f2bb8800a1007a21370a0a27c0d03e9
Tertius Scheepers <Tertiussc@gmail.com>
2020-11-24 07:35:38 +0200

    completed lesson 383: added fail case

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,8 @@ import firebase from 'firebase'
 import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
-    LOGIN_USER_SUCCESS
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL
 } from './types'
 
 export const emailChanged = (text) => {
@@ -20,11 +21,26 @@ export const passwordChanged = (text) => {
     }
 }
 
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    })
+}
+
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL })
+}
+
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(user => {
-                dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSuccess(dispatch, user))
+                    .catch(() => loginUserFail(dispatch))
+
             })
     }
 }
