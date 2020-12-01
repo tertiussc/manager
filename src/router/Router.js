1deb80cca73345d1f590fa9dde8d03c921f9ddc7
Tertius Scheepers <Tertiussc@gmail.com>
2020-12-01 11:56:56 +0200

    completed lesson 393: added navigationRight to title of employee screen

--- src/router/Router.js
+++ src/router/Router.js
@@ -11,7 +11,13 @@ const RouterComponent = () => {
                     <Scene key="login" component={LoginForm} title="Please Login" initial />
                 </Scene>
                 <Scene key="main">
-                    <Scene key="employeeList" component={EmployeeList} title="Employees" />
+                    <Scene
+                        rightTitle="Add"
+                        onRight={() => { console.log('Right') }}
+                        key="employeeList"
+                        component={EmployeeList}
+                        title="Employees"
+                    />
                 </Scene>
             </Stack>
         </Router>
