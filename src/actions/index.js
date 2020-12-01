0768ff3f2602a52481db9ae6f76fba8be2c55f94
Tertius Scheepers <Tertiussc@gmail.com>
2020-12-01 08:27:18 +0200

    completed lesson 391: Hooked up the navigation to login success action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import { Actions } from 'react-native-router-flux'
 
 import {
     EMAIL_CHANGED,
@@ -27,6 +28,7 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     })
+    Actions.employeeList()
 }
 
 const loginUserFail = (dispatch) => {
