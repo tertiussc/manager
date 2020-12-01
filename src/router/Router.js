4772cd5d2c05fc5fa51fdf34526a769936f7370d
Tertius Scheepers <Tertiussc@gmail.com>
2020-12-01 08:17:34 +0200

    completed lesson 390: added the employeeList screen and navigated using scene

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Stack, Scene, Router } from 'react-native-router-flux'
 import LoginForm from '../screens/LoginForm'
+import EmployeeList from '../components/EmployeeList'
 
 const RouterComponent = () => {
     return (
         <Router sceneStyle={{}}>
-            <Stack key="root">
-                <Scene key="login" component={LoginForm} title="Please Login" />
+            <Stack key="root" headerLayoutPreset={'center'}>
+                <Scene key="login" component={LoginForm} title="Please Login" initial />
+                <Scene key="employeeList" component={EmployeeList} title="Employees" />
             </Stack>
         </Router>
 
