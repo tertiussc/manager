4c8ac194df1bd547b84a5ba1e4562f80bd471577
Tertius Scheepers <Tertiussc@gmail.com>
2020-12-14 08:03:01 +0200

    completed lesson 410: rendered employee list with flatlist. Q&A work arround

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase'
 import { Actions } from 'react-native-router-flux'
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types'
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_FETCH_SUCCESS } from './types'
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
@@ -18,6 +18,17 @@ export const employeeCreate = ({ name, phone, shift }) => {
             .then(() => {
                 dispatch({ type: EMPLOYEE_CREATE })
                 Actions.pop({ type: 'reset' })
+            })
+    }
+}
+
+export const employeeFetch = () => {
+    const { currentUser } = firebase.auth()
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() })
             })
     }
 }
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, ListItem, StyleSheet } from 'react-native';
+import { connect } from 'react-redux'
+import { employeeFetch } from '../actions/EmployeeActions'
 
 class EmployeeList extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
-        );
+    componentDidMount() {
+        this.props.employeeFetch();
     }
 
-}
+    createDataSource(employees) {
+        let employeeArr = [];
+        if (employees) {
+            employeeArr = Object.keys(employees).map(function (k, y) {
+                let empObj = employees[k]
+                empObj.key = k;
+                return empObj;
+            })
+        }
+        return (
+            <FlatList
+                data={employeeArr}
+                renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+            />
+        )
+    }
 
-export default EmployeeList;
+    render() {
+        if (this.props.employees) {
+            return (
+                <View>
+                    {this.createDataSource(this.props.employees)}
+                </View>
+            )
+        }
+
+    }
+}
+const mapStateProps = state => {
+    const employees = state.employees
+    return { employees };
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
+export default connect(mapStateProps, { employeeFetch })(EmployeeList);;
